e4eb131230c4f41ddb58f50a243d7bfd1b59e8f2
akarras <chw111111@hotmail.com>
2019-07-01 20:44:44 -0600

    Fixed unit tests and hopefully fixed projects not being included in solution

--- CaliSpeed/wwwroot/js/calispeed.js
+++ CaliSpeed/wwwroot/js/calispeed.js
@@ -1,6 +1,28 @@
-﻿let type = "WebGL";
-if (!PIXI.utils.isWebGLSupported()) {
-  type = "canvas";
-}
+﻿document.onload = function () {
 
-PIXI.utils.sayHello(type);
+  let type = "WebGL";
+  if (!PIXI.utils.isWebGLSupported()) {
+    type = "canvas";
+  }
+
+  PIXI.utils.sayHello(type);
+
+  const app = new PIXI.Application({
+    autoResize: true,
+    resolution: devicePixelRatio
+  });
+  document.body.appendChild(app.view);
+
+  app.renderer.backgroundColor = 0x061639;
+
+  app.renderer.autoResize = true;
+  app.renderer.resize(512, 512);
+
+  app.renderer.view.style.position = "absolute";
+  app.renderer.view.style.display = "block";
+  app.renderer.autoResize = true;
+  app.renderer.resize(window.innerWidth, window.innerHeight);
+
+
+
+}
