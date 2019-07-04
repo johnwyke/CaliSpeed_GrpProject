73a5693ec3ab12ed5904aeb836bb5327ffcbd7f5
kameronsearle <kameronsearle@mail.weber.edu>
2019-07-03 17:45:25 -0700

    Kameron1 (play card example) (#2)
    * SignalR Example
    
    Added Signal R Library and configured in project.
    
    Live example enabled: Any number of players can load the site, enter their name, and click "Play Card". Every other player will see an update that says "[user] played a card".
    
    * Separate SignalR Page; .gitignore removed
    
    Created separate page for SignalR example.
    
    Removed .gitignore file

---
+++ CaliSpeed/SignalR_Hubs/ExampleHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CaliSpeed.SignalR_Hubs
+{
+    public class ExampleHub : Hub
+    {
+        public async Task PlayCard(string user)
+        {
+            await Clients.All.SendAsync("ReceiveCard", user);
+        }
+    }
+}
---
+++ CaliSpeed/wwwroot/js/example.js
@@ -0,0 +1,50 @@
+﻿"use strict";
+
+// builds connection with ExampleHub
+var connection = new signalR.HubConnectionBuilder().withUrl("/ExampleHub").build();
+
+// Disables send button until connection is established
+document.getElementById("playButton").disabled = true;
+
+// #region Starts connection, enables send button
+connection
+.start()
+.then
+(
+    function ()
+    {
+        document.getElementById("playButton").disabled = false;
+    }
+);
+
+// #endregion
+
+// #region Listens for click, runs function to write "[user] played card"
+document
+    .getElementById("playButton")
+    .addEventListener
+    (
+        "click",
+        function (event)
+        {
+            var user = document.getElementById("playerName").value;
+            connection.invoke("PlayCard", user);
+        }
+    );
+
+// #endregion
+
+// #region Writes "[user] played card"
+connection.on
+(
+    "ReceiveCard",
+    function (user)
+    {
+        var encodedMsg = user + " played a card";
+        var li = document.createElement("li");
+        li.textContent = encodedMsg;
+        document.getElementById("gameUpdatesList").appendChild(li);
+    }
+);
+
+// #endregion
