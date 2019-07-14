f3fa1ba6a26f9b558f983e6c4a034991d8632781
adkarras <chw111111@hotmail.com>
2019-07-13 18:11:57 -0600

    Added more documentation and refactored to clear things up

---
+++ CaliSpeed/Tasks.md
@@ -0,0 +1,8 @@
+﻿# CaliSpeed TODO:
+_Note this list does not include things left to be done in the CaliforniaSpeedLibrary project_
+* Create a conversion between the card enum and the card object in the javascript. Conversion can happen in C# or Javascript
+* Handle new card event in Javascript, likely will need to call reset position to snap the player card back
+* Handle new board event in Javascript. Will need to adjust how the sprites are loaded
+* Investigate websocket errors when running on calispeed.azurewebsites.net, (Aaron has to do this)
+* Room system (optional)
+* UI enhancements (optional)
--- calispeed/SignalRHubs/GameHub.cs
+++ calispeed/SignalRHubs/GameHub.cs
@@ -6,15 +6,34 @@ namespace CaliSpeed.SignalRHubs
 {
     public class GameHub : Hub
     {
+        /// <summary>
+        ///  Container class that encapsulates all data that the client needs for a new play
+        /// </summary>
         public class CardPlay
         {
+            /// <summary>
+            /// Incoming card
+            /// </summary>
             public Game.Card Card;
+            /// <summary>
+            /// Row that the play takes place at
+            /// </summary>
             public int Row;
+            /// <summary>
+            /// Column that the play takes place at
+            /// </summary>
             public int Column;
         }
 
         private static Game game;
 
+        /// <summary>
+        /// Allows a client to request that their card be played.
+        /// Sends a play result back indicating whether their play succeeded or not.
+        /// </summary>
+        /// <param name="row">Row that the card should be placed</param>
+        /// <param name="column">Column that the card should be placed</param>
+        /// <returns></returns>
         public async Task PlayCard(int row, int column)
         {
             var game = GetGame();
@@ -28,6 +47,10 @@ namespace CaliSpeed.SignalRHubs
             }
         }
 
+        /// <summary>
+        /// The client has asked for all cards to be received, send them.
+        /// </summary>
+        /// <returns></returns>
         public async Task GetCardsList()
         {
             var game = GetGame();
@@ -40,18 +63,25 @@ namespace CaliSpeed.SignalRHubs
             if (game == null)
             {
                 game = new Game();
+                // Bind events to Game events
+                // See CaliforniaSpeedLibrary::Game.cs to see further documentation
                 game.NewBoardEvent += Game_NewBoardEvent;
-                game.NewCardEvent += Game_CardPlayedEvent;
+                game.NewCardPlayedEvent += Game_NewCardPlayedEvent;
             }
             return game;
         }
 
+        /// <summary>
+        /// Callback function that allows 
+        /// </summary>
+        /// <param name="cards">2x4 array containing the cards</param>
+        /// <returns></returns>
         private async Task Game_NewBoardEvent(Game.Card[,] cards)
         {
             await Clients.All.SendAsync("AllCards", cards);
         }
 
-        private async Task Game_CardPlayedEvent(int player, int row, int column, Game.Card card)
+        private async Task Game_NewCardPlayedEvent(int player, int row, int column, Game.Card card)
         {
             CardPlay play = new CardPlay() { Card = card, Column = column, Row = row };
             await Clients.All.SendAsync("NewCard", play);
